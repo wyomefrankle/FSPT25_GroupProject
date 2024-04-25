8edca6a54a4c364fdf52070cdc70f04ba95d4fb7
Wyome Frankle <wyomefrankle1704@gmail.com>
2024-04-25 17:51:57 +0200

    Improved Gemini AI implementation in the backend

--- routes/users.js
+++ routes/users.js
@@ -10,22 +10,26 @@ var router = express.Router();
 const {API_KEY} = process.env; //storing in env variable for security reasons
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-// Access your API key as an environment variable (see "Set up your API key" above)
+// Access API key as an environment variable
 const genAI = new GoogleGenerativeAI(API_KEY);
-// const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-
-// For text-only input, use the gemini-pro model
 const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
-async function run() {
-  const prompt = "Give me recommendations for skincare products for oily skin that is available in the US and less than 20 USD and accessible in most stores. I want 3 product recommendations for a cleanser, 3 recommendations for a toner, 3 recommendations for a serum, 3 recommendations for a face cream. Note: Please make the output be in an easy to use object format that I can post to the backend of a full stack js app that has an SQL table like this: CREATE TABLE SkincareProducts (id INT PRIMARY KEY AUTO_INCREMENT, product_type VARCHAR(50) NOT NULL, brand VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, price DECIMAL(5,2) NOT NULL, description TEXT, imageURL TEXT);"
+//AI response to skincare quiz for user
+router.get('/', async (req, res) => {
+  try {
+  const { skintype, budget, country, skinconcern } = req.query;
+  const prompt = `Give me recommendations for skincare products for ${skintype} skin that targets ${skinconcern} that is available in the ${country} and less than ${budget} USD and accessible in most stores. I want 3 product recommendations for a cleanser, 3 recommendations for a toner, 3 recommendations for a serum, 3 recommendations for a face cream. Note: Please make the output be in an easy to use object format that I can post to the backend of a full stack js app that has an SQL table like this: CREATE TABLE SkincareProducts (id INT PRIMARY KEY AUTO_INCREMENT, product_type VARCHAR(50) NOT NULL, brand VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, price DECIMAL(5,2) NOT NULL, description TEXT, imageURL TEXT);`
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
   const text = response.text();
-  console.log(text);
+  res.json( {text} );
+  console.log({text}) //if i want to see the text returned by Gemini in terminal
+} catch (error) {
+  console.error('Error using Gemini AI:', error);
+  response.status(500).json({ error: 'An error occurred while using Gemini AI' });
 }
+});
 
-run();
 
 module.exports = router;
