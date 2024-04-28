1a482e6bf55de7a28b157956c021140473720c4e
MEG <MEGHCHARY@GMAIL.COM>
2024-04-27 18:05:34 -0700

    Added bootstrap links for styling. Created an images folder and added an image to potentially be used as the favicon. Added a new component NewUser.

---
+++ client/src/components/NewUser.jsx
@@ -0,0 +1,121 @@
+import { useState } from "react";
+import Modal from "./Modal";
+function NewUser() {
+  //   const [user_id, setUser_id] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [skintype, setSkintype] = useState("");
+
+  const handleCreateAccount = () => {
+    fetch("/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        // user_id: user_id,
+        firstname: firstname,
+        lastname: lastname,
+        password: password,
+        email: email,
+        skintype: skintype,
+      }),
+    })
+      .then((result) => {
+        if (!result.ok) {
+          throw new Error("Error creating user account");
+        }
+        return result.json();
+      })
+      .then((json) => {
+        // Handle the success response from the server, e.g., show a success message
+        console.log("User account created successfully:", json);
+      })
+      .catch((error) => {
+        // Handle errors, e.g., show an error message to the user
+        console.error("Error creating user account:", error.message);
+      });
+    // Clear the form inputs after successful submission
+    // setUser_id("");
+    setFirstname("");
+    setLastname("");
+    setPassword("");
+    setEmail("");
+    setSkintype("");
+  };
+  return (
+    <div className="App" style={{ width: "30rem" }}>
+      <h1 className="title">Create account</h1>
+      <form className="form">
+        {/* <div>
+          <label className="form-label">Username:</label>
+          <input
+            type="text"
+            value={user_id}
+            onChange={(e) => setUser_id(e.target.value)}
+            className="form-control"
+            style={{ width: "40%" }}
+          />
+        </div> */}
+        <div>
+          <label className="form-label">First Name:</label>
+          <input
+            type="firstname"
+            value={firstname}
+            onChange={(e) => setFirstname(e.target.value)}
+            className="form-control"
+            style={{ width: "40%" }}
+          />
+        </div>
+        <div>
+          <label className="form-label">Last Name:</label>
+          <input
+            type="lastname"
+            value={lastname}
+            onChange={(e) => setLastname(e.target.value)}
+            className="form-control"
+            style={{ width: "40%" }}
+          />
+        </div>
+        <div>
+          <label className="form-label">Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="form-control"
+            style={{ width: "40%" }}
+          />
+        </div>
+        <div>
+          <label className="form-label">Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="form-control"
+            style={{ width: "40%" }}
+          />
+        </div>
+        <div>
+          <label className="form-label">Skin Type:</label>
+          <input
+            type="skintype"
+            value={skintype}
+            onChange={(e) => setSkintype(e.target.value)}
+            className="form-control"
+            style={{ width: "40%" }}
+          />
+        </div>
+        <Modal
+          launchBtnText="Create Account"
+          modalTitle="User successfully created! "
+          handleCreateAccount={handleCreateAccount}
+        />
+      </form>
+    </div>
+  );
+}
+export default NewUser;
